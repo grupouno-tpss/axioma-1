f7536ff897add296f7fa2d561b92d8cd0ba29684
grupouno-tpss <88520777+grupouno-tpss@users.noreply.github.com>
2021-08-09 15:30:40 -0500

    Update start.js

--- javascript/start.js
+++ javascript/start.js
@@ -4,7 +4,7 @@ var n = 0;
 startButton.addEventListener("click", function int () {
     document.getElementById("audio").play();
     location.href = window.location + "#h" + n;
-    window.history.replaceState({}, "hola", "file:///C:/xampp/htdocs/axioma1/index.html");
+    window.history.replaceState({}, "hola", "https://grupouno-tpss.github.io/axioma-1/");
     startButton.innerHTML = "En progreso";
     if (n == 0) {
         alertInfo("<br>"+ "<br>" + "A continuación, vas a encontrar una serie de preguntas o planteamientos, los cuales, tienen el proposito de hacerte pensar o repasar sore las formas de comunicacion en diferentes contexto (especialmente del axioma 1)." + "<br>" + "<br>"+ "Las respuestas las hemos seleccionado a partir de la opinion de otras personas y logica propia." + "<br>"+ "<br>");
